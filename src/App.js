2a2ed55fedae698c4c0165fd8b03a479dceb905a
Cristina SG <cristinasanchezg@gmail.com>
2022-11-07 12:00:59 -0600

    componente1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import MiComponente from "./Components/MiComponente";
 
 
 function App() {
   return (
-   <h1>Proyecto react limpio</h1>
-  );
+    <>
+    <h1>Proyecto react limpio</h1>
+   <MiComponente/>
+    </>
+   );
 }
 
 export default App;

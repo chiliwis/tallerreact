bca0283c06d2e20bd9b41438788deefc318319a9
Cristina SG <cristinasanchezg@gmail.com>
2022-11-07 12:45:32 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MiComponente from "./Components/MiComponente";
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
     <>
+    <Navbar/>
     <h1>Proyecto react limpio</h1>
    <MiComponente/>
     </>

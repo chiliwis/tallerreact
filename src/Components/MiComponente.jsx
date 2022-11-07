845f59534262c42f350e0274403626be564f9dcf
Cristina SG <cristinasanchezg@gmail.com>
2022-11-07 12:27:35 -0600

    componentes

--- src/Components/MiComponente.jsx
+++ src/Components/MiComponente.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const MiComponente = () => {
   return (
-    <div>MiComponente</div>
+<>
+<h3>Hola, soy un componente</h3>
+<h4>Taller de recuperacion</h4>
+</>
+
   )
 }
 
